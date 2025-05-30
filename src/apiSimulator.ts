4d8b9d1c795ec2446546559e265e3a2e1abf83f4
dmfiel <dave@fiel.us>
2025-05-30 11:43:42 -0400

    completed retry functionality

--- src/apiSimulator.ts
+++ src/apiSimulator.ts
@@ -1,3 +1,5 @@
+const chanceSuccess = 0.8;
+
 export interface Product {
   id: number;
   name: string;
@@ -7,17 +9,33 @@ export interface Review {
   id: number;
   review: string;
 }
+export interface Report {
+  totalSales: number;
+  unitsSold: number;
+  averagePrice: number;
+}
+
+export class NetworkError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+export class DataError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
 
 export function fetchProductCatalog(): Promise<Product[]> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (Math.random() < 0.8) {
+      if (Math.random() < chanceSuccess) {
         resolve([
           { id: 1, name: 'Laptop', price: 1200 },
           { id: 2, name: 'Headphones', price: 200 }
         ]);
       } else {
-        reject('Failed to fetch product catalog');
+        reject(new NetworkError('Failed to fetch product catalog'));
       }
     }, 1000);
   });
@@ -26,14 +44,62 @@ export function fetchProductCatalog(): Promise<Product[]> {
 export function fetchProductReviews(productId: number): Promise<Review[]> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (Math.random() < 0.8) {
-        resolve([
-          { id: 1, review: 'This is great.' },
-          { id: 2, review: 'It burned down my house.' }
-        ]);
+      if (Math.random() < chanceSuccess) {
+        resolve(
+          [
+            { id: 1, review: 'This is great.' },
+            { id: 1, review: 'It burned down my house.' },
+            { id: 2, review: 'Why did I buy this?' },
+            { id: 2, review: 'It cleaned my house!' }
+          ].filter(review => review.id === productId)
+        );
       } else {
-        reject('Failed to fetch product reviews');
+        reject(
+          new NetworkError(
+            `Failed to fetch reviews for product ID ${productId}`
+          )
+        );
       }
     }, 1500);
+  });
+}
+
+export function fetchSalesReport(): Promise<Report> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < chanceSuccess) {
+        resolve({ totalSales: 525.25, unitsSold: 5, averagePrice: 150 });
+      } else {
+        reject(new DataError(`Failed to fetch sales report`));
+      }
+    }, 1000);
+  });
+}
+
+export function retryPromise(
+  target: Function,
+  retries: number,
+  delay: number
+): Promise<any> {
+  return new Promise((resolve, reject) => {
+    target()
+      .then((result: any) => resolve(result))
+      .catch((error: Error) => {
+        if (retries === 0) reject(error);
+        else {
+          console.log(
+            `Retrying ${
+              target.name ? target.name : 'fetchProductReviews'
+            }: ${retries}`
+          );
+          setTimeout(
+            () =>
+              retryPromise(target, retries - 1, delay)
+                .then((result: any) => resolve(result))
+                .catch(error => reject(error)),
+            delay
+          );
+        }
+      });
   });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
-import { fetchProductCatalog, fetchProductReviews } from './apiSimulator';
+import {
+  fetchProductCatalog,
+  fetchProductReviews,
+  fetchSalesReport,
+  retryPromise,
+  Product
+} from './apiSimulator';
+
+const maxRetries = 3;
+const retryDelay = 1000;
 
 function runFetchReport() {
-  try {
-    fetchProductCatalog()
-      .then(products => console.log(products))
-      .catch(err => {
-        throw new Error(err);
+  retryPromise(fetchProductCatalog, maxRetries, retryDelay)
+    .then(products => {
+      console.log(products);
+      (products as Product[]).forEach((product, index) => {
+        retryPromise(
+          () => fetchProductReviews(product.id),
+          maxRetries,
+          retryDelay
+        )
+          .then(reviews => console.log(reviews))
+          .catch(err => console.error(err))
+          .finally(() => {
+            if (index === products.length - 1) {
+              retryPromise(fetchSalesReport, maxRetries, retryDelay)
+                .then(report => console.log(report))
+                .catch(err => console.error(err));
+            }
+          });
       });
-    fetchProductReviews(1)
-      .then(reviews => console.log(reviews))
-      .catch(err => {
-        throw new Error(err);
-      });
-  } catch (err) {
-    console.error(err);
-  }
+    })
+    .catch(err => console.error(err))
+    .finally(() => console.log('All API calls have been attempted'));
 }
 
-console.log('start');
 runFetchReport();
-console.log('end');
